ebc1dfd83dd5b1971c83e2bf8591450a62ca5d24
valje311 <xiaomimia2vale@gmail.com>
2025-07-10 20:49:11 +0200

    Add files via upload

--- CSV_to_SQL.py
+++ CSV_to_SQL.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 import mysql.connector
 
 config = configparser.ConfigParser()
-config.read('MyConfig')
+config.read('MyConfig.ini')
 
 CONNECTION_STRING = f"mysql+mysqlconnector://{config['SQL']['username']}:{config['SQL']['password']}@localhost:{config['SQL']['port']}/{config['SQL']['database']}"
 engine = create_engine(CONNECTION_STRING)
 
-for chunk in pd.read_csv(config['CSV']['file_path'], chunksize=int(config['CSV']['chunk_size']), sep=";"): 
-    chunk['Timestamp'] = pd.to_datetime(chunk['Timestamp'], format='%Y.%m.%d %H:%M:%S').astype(int) // 10**6
-    chunk.to_sql(config['SQL']['table_name'], engine, if_exists='append', index=False)
+for chunk in pd.read_csv(config['CSV']['filepath'], chunksize=int(config['CSV']['chunksize']), sep=";"): 
+    chunk['Timestamp'] = pd.to_datetime(chunk['Timestamp'], format='%Y.%m.%d %H:%M:%S.%f').astype('int64') // 10**6
+    chunk.to_sql(config['SQL']['tablename'], engine, if_exists='append', index=False)
